ca7d0711a3c776631cfa63992dbf7f1c6881c168
Pr4veenn <praveen.tech.js@gmail.com>
2025-04-28 20:20:58 +0530

    ethods

--- practice/javascript.js
+++ practice/javascript.js
@@ -1,441 +1,441 @@
   
 
-  // callback ///
+//   // callback ///
 
-  function call(){
-    console.log("call me");
-  }
+//   function call(){
+//     console.log("call me");
+//   }
 
-  function main(callback){
-    console.log("first");
+//   function main(callback){
+//     console.log("first");
 
-    setTimeout(()=>{
+//     setTimeout(()=>{
         
-        callback();
-        console.log("finish");
-    },3000)
+//         callback();
+//         console.log("finish");
+//     },3000)
     
-  }
+//   }
 
-  main(call)
+//   main(call)
 
-  function call(){
-    console.log("call me..");
+//   function call(){
+//     console.log("call me..");
     
-  }
+//   }
 
-  function main (callback){
+//   function main (callback){
 
-    console.log('start')
+//     console.log('start')
 
-    setTimeout(()=>{
-        callback ()
-        console.log("finish")
-    },3000)
-  }
+//     setTimeout(()=>{
+//         callback ()
+//         console.log("finish")
+//     },3000)
+//   }
 
-  main(call)
+//   main(call)
 
 
-  function factorial(n){
+//   function factorial(n){
      
-    let result=1;
-    for(let i=1;i<=n;i++){
-        result *= i;
-    }
-    return result;
-  }
+//     let result=1;
+//     for(let i=1;i<=n;i++){
+//         result *= i;
+//     }
+//     return result;
+//   }
 
-  console.log(factorial(10));
+//   console.log(factorial(10));
 
-  const person = {
-    name: "John",
-    greet: function (message) {
-      console.log(this.name +" says : "+ message );
-    },
-  };
+//   const person = {
+//     name: "John",
+//     greet: function (message) {
+//       console.log(this.name +" says : "+ message );
+//     },
+//   };
   
-  const anotherPerson = {
-    name: "Alice",
-  };
+//   const anotherPerson = {
+//     name: "Alice",
+//   };
   
-  person.greet.call(anotherPerson, " Hello!"); // Alice says: Hello!
+//   person.greet.call(anotherPerson, " Hello!"); // Alice says: Hello!
 
-  'use strict';
+//   'use strict';
 
-  var num = 01234;
+//   var num = 01234;
 
-  console.log(num);
+//   console.log(num);
 
 
-  function sum(...numbers){
-    let result=0;
-    for(let num of numbers){
-      result+= num;
-    }
-    return result;
-  }
+//   function sum(...numbers){
+//     let result=0;
+//     for(let num of numbers){
+//       result+= num;
+//     }
+//     return result;
+//   }
 
-  console.log(sum(1,2,3));
+//   console.log(sum(1,2,3));
 
-  const abc={
-    var x=10,
-    fn : function(){
-        console.log(this.name)
-    }
-};
-abc.fn();
+//   const abc={
+//     var x=10,
+//     fn : function(){
+//         console.log(this.name)
+//     }
+// };
+// abc.fn();
 
 
-////////////////
+// ////////////////
 
 
-const person = {
-  name: 'John',
-  sayName: function () {
-    console.log(this.name); // Refers to the 'person' object
-  },
-};
+// const person = {
+//   name: 'John',
+//   sayName: function () {
+//     console.log(this.name); // Refers to the 'person' object
+//   },
+// };
 
-person.sayName(); // Outputs 'John'
+// person.sayName(); // Outputs 'John'
 
 
-/ DEEP COPY ////
+// / DEEP COPY ////
 
 
-let array = [1,2,3,4]
+// let array = [1,2,3,4]
 
-let deep = JSON.parse(JSON.stringify(array))
+// let deep = JSON.parse(JSON.stringify(array))
 
-deep[2]=10
+// deep[2]=10
 
-console.log(deep)
+// console.log(deep)
 
-console.log(array)
+// console.log(array)
 
-///////////////
+// ///////////////
 
-let array = [5,6,8,7,3,10]
+// let array = [5,6,8,7,3,10]
 
-let smallest= array.reduce((min, num)=>{
-  if(num < min);
-  smallest = num;
-})  
+// let smallest= array.reduce((min, num)=>{
+//   if(num < min);
+//   smallest = num;
+// })  
 
 
-console.log(smallest);
+// console.log(smallest);
 
 
 
-let array = [5, 6, 8, 7, 3, 10];
+// let array = [5, 6, 8, 7, 3, 10];
 
-let smallest = array.reduce((min, num) => {
-  return num < min ? num : min;
-}, array[0]); // Initialize with the first element of the array
+// let smallest = array.reduce((min, num) => {
+//   return num < min ? num : min;
+// }, array[0]); // Initialize with the first element of the array
 
-console.log(smallest);
+// console.log(smallest);
 
 
-let round = Math.pow(5,2)
+// let round = Math.pow(5,2)
 
-console.log(round)
+// console.log(round)
 
 
-let random = Math.random()
+// let random = Math.random()
 
-console.log(random)
+// console.log(random)
 
 
-/ INSTANCE ////
+// / INSTANCE ////
 
-function car( make, model){
+// function car( make, model){
 
-  this.make = 1;
-  this.model = 2;
-}
+//   this.make = 1;
+//   this.model = 2;
+// }
 
-let mycar = new car('toyota' , 'fortuner')
+// let mycar = new car('toyota' , 'fortuner')
 
-console.log(mycar instanceof car)
+// console.log(mycar instanceof car)
 
 
 
-//Function to add 2 to a number
-function addTwo(x) {
-  return x + 2;
-}
+// //Function to add 2 to a number
+// function addTwo(x) {
+//   return x + 2;
+// }
 
 
-// Function to multiply a number by 3
-function multiplyByThree(x) {
-  return x * 3;
-}
+// // Function to multiply a number by 3
+// function multiplyByThree(x) {
+//   return x * 3;
+// }
 
-// Now, let's compose them to create a new function
-const addTwoAndMultiplyByThree = (x) => multiplyByThree(addTwo(x));
+// // Now, let's compose them to create a new function
+// const addTwoAndMultiplyByThree = (x) => multiplyByThree(addTwo(x));
 
-// You can use the composed function like this:
-const result = addTwoAndMultiplyByThree(5); // Result will be 21 (add 2, then multiply by 3)
+// // You can use the composed function like this:
+// const result = addTwoAndMultiplyByThree(5); // Result will be 21 (add 2, then multiply by 3)
 
-console.log(result)
+// console.log(result)
 
-// FUNCTION COMPOSITION //
+// // FUNCTION COMPOSITION //
 
-const fn1 = (x) => x + 2;
+// const fn1 = (x) => x + 2;
 
-const fn2 = (x) => x * 3;
+// const fn2 = (x) => x * 3;
 
-const compose = (x) => fn2(fn1(x));
+// const compose = (x) => fn2(fn1(x));
 
-const result = compose(6);
+// const result = compose(6);
 
-console.log(result)
+// console.log(result)
 
 
-// HIGHER ORDER FUNCTION ///
+// // HIGHER ORDER FUNCTION ///
 
 
-function operate(a ,b ,operation){
-  return operation(a ,b)
-}
+// function operate(a ,b ,operation){
+//   return operation(a ,b)
+// }
 
-function add(a , b){
-  return a + b;
-}
+// function add(a , b){
+//   return a + b;
+// }
 
-function subtract(a , b){
-  return a - b;
-}
+// function subtract(a , b){
+//   return a - b;
+// }
 
-const result1 = operate(3 , 4 , add);
+// const result1 = operate(3 , 4 , add);
 
-console.log(result1)
+// console.log(result1)
 
-const result2 = operate(7 , 5, subtract)
+// const result2 = operate(7 , 5, subtract)
 
-console.log(result2)
+// console.log(result2)
 
-//////////////////
+// //////////////////
 
-Function that returns a Promise
-const fetchRandomNumber = () => {
-  return new Promise((resolve, reject) => {
-    // Simulating a delay with setTimeout
-    setTimeout(() => {
-      const randomNumber = Math.floor(Math.random() * 100); // Generate a random number between 0 and 99
+// Function that returns a Promise
+// const fetchRandomNumber = () => {
+//   return new Promise((resolve, reject) => {
+//     // Simulating a delay with setTimeout
+//     setTimeout(() => {
+//       const randomNumber = Math.floor(Math.random() * 100); // Generate a random number between 0 and 99
 
-      // Simulating success or failure
-      if (randomNumber > 50) {
-        resolve(randomNumber); // Resolve the Promise with the random number
-      } else {
-        reject("Number is too low!"); // Reject the Promise with an error message
-      }
-    }, 1000); // 1 second delay
-  });
-};
+//       // Simulating success or failure
+//       if (randomNumber > 50) {
+//         resolve(randomNumber); // Resolve the Promise with the random number
+//       } else {
+//         reject("Number is too low!"); // Reject the Promise with an error message
+//       }
+//     }, 1000); // 1 second delay
+//   });
+// };
 
-// Using the Promise
-fetchRandomNumber()
-  .then((number) => {
-    console.log("Fetched random number:", number);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+// // Using the Promise
+// fetchRandomNumber()
+//   .then((number) => {
+//     console.log("Fetched random number:", number);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
 
 
-function fetchUserData() {
-    const fetchUserData = new Promise((resolve, reject) => {
-        console.log("Fetching user data...");
+// function fetchUserData() {
+//     const fetchUserData = new Promise((resolve, reject) => {
+//         console.log("Fetching user data...");
 
-        setTimeout(() => {
-            const success = true; 
+//         setTimeout(() => {
+//             const success = true; 
 
-            if (success) {
-                const userData = { name: "Alice", age: 30 };
-                resolve(userData);
-            } else {
-                reject("Failed to fetch user data");
-            }
-        }, 2000);
-    });
+//             if (success) {
+//                 const userData = { name: "Alice", age: 30 };
+//                 resolve(userData);
+//             } else {
+//                 reject("Failed to fetch user data");
+//             }
+//         }, 2000);
+//     });
 
 
 
-fetchUserData
-    .then(data => {
-        console.log("User Data:", data); 
-    })
-    .catch(error => {
-        console.log("Error:", error); 
-    });
+// fetchUserData
+//     .then(data => {
+//         console.log("User Data:", data); 
+//     })
+//     .catch(error => {
+//         console.log("Error:", error); 
+//     });
 
 
 
 
-   Function that returns a Promise
-function fetchUserData  () {
-    return new Promise((resolve, reject) => {
-        console.log("Fetching user data...");
+//    Function that returns a Promise
+// function fetchUserData  () {
+//     return new Promise((resolve, reject) => {
+//         console.log("Fetching user data...");
 
-        setTimeout(() => {
-            const success = true; 
+//         setTimeout(() => {
+//             const success = true; 
 
-            if (success) {
-                const userData = { name: "Alice", age: 30 };
-                resolve(userData);
-            } else {
-                reject("Failed to fetch user data");
-            }
-        }, 2000);
-    });
-};
+//             if (success) {
+//                 const userData = { name: "Alice", age: 30 };
+//                 resolve(userData);
+//             } else {
+//                 reject("Failed to fetch user data");
+//             }
+//         }, 2000);
+//     });
+// };
 
-// Async function to fetch user data
-async function async ()  {
-    try {
-        const data = await fetchUserData(); // Wait for the Promise to resolve
-        console.log("User Data:", data);
-    } catch (error) {
-        console.log("Error:", error);
-    }
-};
+// // Async function to fetch user data
+// async function async ()  {
+//     try {
+//         const data = await fetchUserData(); // Wait for the Promise to resolve
+//         console.log("User Data:", data);
+//     } catch (error) {
+//         console.log("Error:", error);
+//     }
+// };
 
-// Call the async function
-async();
+// // Call the async function
+// async();
 
 
-function data(){
-  return new Promise((res,rej) =>{
-    console.log("fetching data....");
+// function data(){
+//   return new Promise((res,rej) =>{
+//     console.log("fetching data....");
 
-    setTimeout(()=>{
-        const success = true;
-        if(success){
-          const userdata ={name:"praveen",age:25}
-          res(userdata)
-        }
-        else{
-          rej("eroor occured")
-        }
-    },3000)
-  })
-};
+//     setTimeout(()=>{
+//         const success = true;
+//         if(success){
+//           const userdata ={name:"praveen",age:25}
+//           res(userdata)
+//         }
+//         else{
+//           rej("eroor occured")
+//         }
+//     },3000)
+//   })
+// };
 
-async function async(){
-  try{
-    const result = await data()
-    console.log(result)
-  }
-  catch(error){
-    console.log(error);
+// async function async(){
+//   try{
+//     const result = await data()
+//     console.log(result)
+//   }
+//   catch(error){
+//     console.log(error);
   
-  }
-}
+//   }
+// }
 
-async()
-data()
-.then(data=>{
-  console.log("userdata : ",data)
-})
-.catch(error=>{
-  console.log(error)
-})
+// async()
+// data()
+// .then(data=>{
+//   console.log("userdata : ",data)
+// })
+// .catch(error=>{
+//   console.log(error)
+// })
 
-//IIFE ///
+// //IIFE ///
 
-let x = 30;
+// let x = 30;
 
-(function fn(){
-    let x = 20;
-    console.log(x)
-})();
+// (function fn(){
+//     let x = 20;
+//     console.log(x)
+// })();
 
-console.log(x);
+// console.log(x);
 
-BLOCK SCOPE ///
+// BLOCK SCOPE ///
 
-function demo(){
+// function demo(){
 
-    if(true){
-        let x="heloo"
-        console.log(x);
-    }
-    console.log(x)
-}
+//     if(true){
+//         let x="heloo"
+//         console.log(x);
+//     }
+//     console.log(x)
+// }
 
-demo()
+// demo()
 
-const numbers = [1,2,3,4,5]
+// const numbers = [1,2,3,4,5]
 
-const output = numbers.reduce(function(num,num2){
-    return num + num2;
-})
+// const output = numbers.reduce(function(num,num2){
+//     return num + num2;
+// })
 
-console.log(output)
+// console.log(output)
 
 
-GENERATOR ///
+// GENERATOR ///
 
-function* myGenerator() {
-    yield 1; // This is a pause point
-    yield 2;
-    yield 3;
-  }
+// function* myGenerator() {
+//     yield 1; // This is a pause point
+//     yield 2;
+//     yield 3;
+//   }
   
-  const generator = myGenerator();
+//   const generator = myGenerator();
   
-  console.log(generator.next()); // { value: 1, done: false }
-  console.log(generator.next()); // { value: 2, done: false }
-  console.log(generator.next()); // { value: 3, done: false }
-  console.log(generator.next()); // { value: undefined, done: true }
+//   console.log(generator.next()); // { value: 1, done: false }
+//   console.log(generator.next()); // { value: 2, done: false }
+//   console.log(generator.next()); // { value: 3, done: false }
+//   console.log(generator.next()); // { value: undefined, done: true }
 
 
-// print 1 to 100 generator////
+// // print 1 to 100 generator////
 
-  function* generatorfn(){
-    for(let i=1;i<=100;i++){
-        yield i;
-    }
-  }
-  const gererator =generatorfn();
+//   function* generatorfn(){
+//     for(let i=1;i<=100;i++){
+//         yield i;
+//     }
+//   }
+//   const gererator =generatorfn();
 
-  for(const aaa of gererator){
-    console.log(aaa)
-  }
+//   for(const aaa of gererator){
+//     console.log(aaa)
+//   }
 
-  for(let i=1;i<=100;i++){
-    console.log(gererator.next().value)
-  }
+//   for(let i=1;i<=100;i++){
+//     console.log(gererator.next().value)
+//   }
 
-// setIntervel ///
-let count=0
+// // setIntervel ///
+// let count=0
 
-console.log("start")
+// console.log("start")
 
-const intervelId = setInterval(function(){
+// const intervelId = setInterval(function(){
 
-    count ++
-    console.log(count)
+//     count ++
+//     console.log(count)
 
-    if(count >=5){
-        clearInterval(intervelId)
-        console.log("end...")
-    }
-},2000);
+//     if(count >=5){
+//         clearInterval(intervelId)
+//         console.log("end...")
+//     }
+// },2000);
 
 
-// CALL METHOD ///
+// // CALL METHOD ///
 
-const person={
-  name : "praveen",
-  greet : function(message){
-    console.log(this.name+ " says " +message)
-  },
-};
+// const person={
+//   name : "praveen",
+//   greet : function(message){
+//     console.log(this.name+ " says " +message)
+//   },
+// };
 
-const person2 ={
-  name : "ajay",
-};
+// const person2 ={
+//   name : "ajay",
+// };
 
-person.greet.apply(person, ["hello."]);
+// person.greet.apply(person, ["hello."]);
