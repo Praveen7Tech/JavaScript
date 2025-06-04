d314c8e96dbbc3f8305b2ca09e00b60f303510d9
Pr4veenn <praveen.tech.js@gmail.com>
2025-06-04 11:06:10 +0530

    practice

--- practice/practice.js
+++ practice/practice.js
@@ -80,3 +80,90 @@
 
 // B();
 
+///////////////////////
+
+// 5 . nested functions
+
+function func1(a){
+    console.log(a)
+    return function func2(b){
+        console.log(b)
+        return function func3(c){
+            console.log(c)
+            return function func4(d){
+                console.log(d)
+                return a + b + c + d
+            }
+        }
+    }
+}
+
+
+console.log(func1(1)(2)(3)(4))
+
+/////////////////////////////////
+
+// 6 . this keyword working .call
+
+const Batman = {
+    name : "Batman",
+    message : function(){
+        return `I am ${this.name}, ${this.name === "Batman" ? "Guaerdian of the Gotham City" : "Why so Serious"}`
+    }
+}
+
+const Joker ={name : "Joker"}
+
+console.log(Batman.message())
+
+////////////////////////////
+
+
+Promise.resolve(1)
+  .finally((data) => {
+    console.log(data);
+    return Promise.reject('error');
+  })
+  .catch((error) => {
+    console.log(error);
+    throw 'error2';
+  })
+  .finally((data) => {
+    console.log(data);
+    return Promise.resolve(2).then(console.log);
+  })
+  .then(console.log)
+  .catch(console.log);
+
+// const promise1 = Promise.resolve(1);
+// const promise2 = Promise.resolve(2);
+// const promise3 = Promise.resolve(3);
+// const promise4 = Promise.reject(4);
+
+// const promiseAll = async () => {
+//   const group1 = await Promise.all([promise1, promise2]);
+//   const group2 = await Promise.all([promise3, promise4]);
+//   return [group1, group2];
+// };
+
+// promiseAll().then(console.log).catch(console.log);
+
+////////////////////////////////
+
+const users = [
+{ firstName: "Alok", lastName: "Raj", age: 23 },
+{ firstName: "Ashish", lastName: "Kumar", age: 29 },
+{ firstName: "Ankit", lastName: "Roy", age: 29 },
+{ firstName: "Pranav", lastName: "Mukherjee", age: 50 },
+];
+
+users.map((user)=> {
+    if(user.age < 30){
+        console.log(user.firstName)
+    }
+})
+
+let x = users.filter((user) => user.age < 30)
+x.map((n)=>{
+    console.log(n.firstName)
+})
