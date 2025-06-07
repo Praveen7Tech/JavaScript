fc175ee3b476e17b21db924b616ab22db886175b
Pr4veenn <praveen.tech.js@gmail.com>
2025-06-07 19:18:08 +0530

    recursive binary search

--- QA/DSA.js
+++ QA/DSA.js
@@ -22,3 +22,29 @@ function binarySearch(arr, target) {
 let numbers = [10, 20, 30, 40, 50, 60, 70];
 console.log(binarySearch(numbers, 50)); 
 console.log(binarySearch(numbers, 25)); 
+
+// recursive binary search
+
+function recursiveBinary(arr, target){
+    return search(arr,target,0, arr.length - 1)
+}
+
+function search(arr,target,leftIndex,rightIndex){
+    if(leftIndex > rightIndex){
+        return -11
+    }
+
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+    
+    if(target === arr[middleIndex]){
+        return middleIndex
+    }
+
+    if(target < arr[middleIndex]){
+        return search(arr,target,leftIndex,middleIndex - 1)
+    }else{
+        return search(arr,target,middleIndex + 1, rightIndex)
+    }
+}
+
+console.log(recursiveBinary([1,2,3,4,5,6], 7))
