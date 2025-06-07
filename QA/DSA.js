bfe5cdb40cd803c63c85d674e08837078e83342f
Pr4veenn <praveen.tech.js@gmail.com>
2025-06-07 19:18:52 +0530

    linear search

--- QA/DSA.js
+++ QA/DSA.js
@@ -48,3 +48,17 @@ function search(arr,target,leftIndex,rightIndex){
 }
 
 console.log(recursiveBinary([1,2,3,4,5,6], 7))
+
+
+
+// linear search
+
+function linearSearch(arr,target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] === target){
+            return i
+        }
+    }return -1
+}
+
+console.log(linearSearch([1,2,3,4,5], 6));
