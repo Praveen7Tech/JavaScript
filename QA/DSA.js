4332713c08f8e71f89ee9ea6d62ef70ca478c314
Pr4veenn <praveen.tech.js@gmail.com>
2025-06-07 19:16:55 +0530

    binary search

---
+++ QA/DSA.js
@@ -0,0 +1,24 @@
+/// Binary Search
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid; 
+    } else if (arr[mid] < target) {
+      left = mid + 1; 
+    } else {
+      right = mid - 1; 
+    }
+  }
+
+  return -1; 
+}
+
+let numbers = [10, 20, 30, 40, 50, 60, 70];
+console.log(binarySearch(numbers, 50)); 
+console.log(binarySearch(numbers, 25)); 
