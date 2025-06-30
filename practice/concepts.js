79f65a66ec5fdd652a9f0e753327143d53c15854
Pr4veenn <praveen.tech.js@gmail.com>
2025-06-30 16:10:48 +0530

    JS

--- practice/concepts.js
+++ practice/concepts.js
@@ -148,20 +148,105 @@
 
 // REMOVE LARGEST STRING FROM ARRAY
 
-const strings = ["car", "apple", "bikerrrrrrrrrrr", "elephant"];
+// const strings = ["car", "apple", "bikerrrrrrrrrrr", "elephant"];
 
-function removeLarge(arr){
-  let large = 0
-  let index = -1
-  for(let i = 0 ; i< arr.length ; i++){
-    if(arr[i].length > large){
-      large = arr[i].length
-      index = i
-    }
+// function removeLarge(arr){
+//   let large = 0
+//   let index = -1
+//   for(let i = 0 ; i< arr.length ; i++){
+//     if(arr[i].length > large){
+//       large = arr[i].length
+//       index = i
+//     }
+//   }
+
+//   arr.splice(index, 1)
+//   return arr
+// }
+
+// console.log(removeLarge(strings))
+
+///////////////
+
+// MERGE OBJECT WITHOUT REPLACE
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { b: 3, c: 4, d: 5 };
+
+// let merged = {...obj1}
+
+// for(let key in obj2){
+//   if(!(key in merged)){
+//     merged[key] = obj2[key]
+//   }
+// }
+
+// console.log(merged)
+
+
+// GENERATOR FUNCTION
+
+// function* generator(){
+//   yield "step 1";
+//   yield "step 2";
+//   yield "step 3";
+//   yield "step 4"
+// }
+
+// const gen = generator()
+
+// console.log(gen)
+
+//////////////////////////
+
+// PROXY OBJECT 
+
+// let obj = {name : "Praveen"}
+
+// const proxyObj = new Proxy(obj,{
+//   get(target, prop){
+//     console.log(`getting ${prop}`)
+//     return target[prop]
+//   },
+//   set(target,prop,value){
+//     console.log(`setting ${prop} to ${value}`)
+//     return target[prop] = 25
+//   }
+// })
+
+// console.log(proxyObj.name)
+// proxyObj.age = 25
+// console.log(proxyObj.age)
+
+//////////////////
+
+//DEEP COPY
+
+
+// const obj = {
+//     name :"Praveen",
+//     details:{
+//         place: "ekm"
+//     }
+// }
+
+// let copy = JSON.parse(JSON.stringify(obj))
+
+// copy.name = "Rohith"
+// copy.details.place = "kollam"
+
+// console.log(obj)
+// console.log(copy)
+
+/////////////
+
+// HiGHER ORDER FUNCTION (return another function)
+
+function Higher(num){
+  return function(value){
+    return num * value
   }
-
-  arr.splice(index, 1)
-  return arr
 }
 
-console.log(removeLarge(strings))
+const answer = Higher(5)
+console.log(answer(2))
