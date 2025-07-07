2351206cf063285a4630e242112347d9db0ed870
Pr4veenn <praveen.tech.js@gmail.com>
2025-07-07 20:56:31 +0530

    BST

--- DSA/BST.js
+++ DSA/BST.js
@@ -75,7 +75,107 @@ class BinarySearchTree {
         }
     }
 
-  
+    //DELETE NODE
+    delete(value){
+        this.root = this.deleteNode(this.root, value)
+    }
+
+    deleteNode(root, value){
+        if(root === null){
+            return null
+        }
+        if(value < root.value){
+            root.left = this.deleteNode(root.left, value)
+        }
+        else if(value > root.value){
+            root.right = this.deleteNode(root.right, value)
+        }
+        else{
+            if(!root.left && !root.right){
+                return null
+            }
+            if(!root.left){
+                return root.right
+            }
+            else if(!root.right){
+                return root.left
+            }
+
+            root.value = this.Min(root.right)
+            root.right = this.deleteNode(root.right, root.value)
+        }
+        return root
+    }
+
+    // Height of BST 
+    Height(root){
+        if(root === null){
+            return -1
+        }
+
+        const leftHeight = this.Height(root.left)
+        const rightHeight = this.Height(root.right)
+
+        return 1 + Math.max(leftHeight, rightHeight)
+    }
+
+    // Find Nth largest
+    NthLargest(n){
+        this.count = 0
+        this.nthLargest = null
+        this.FindNthLarge(this.root, n)
+        return this.nthLargest
+    }
+
+    FindNthLarge(root, n){
+        if(!root){
+            return
+        }
+        this.FindNthLarge(root.right,n)
+        this.count++
+
+        if(this.count === n){
+            this.nthLargest = root.value
+        }
+        this.FindNthLarge(root.left, n)
+    }
+
+    
+    // Find Second largest
+    SecondLargest(){
+        if(!this.root || !this.root.left && !this.root.right){
+            return null
+        }
+
+        let current = this.root
+        while(current){
+            if(current.right && !current.right.right && !current.right.left){
+                return current.value
+            }
+
+            if(current.right && !current.right.right && current.right.left){
+                return this.Max(current.right.left)
+            }
+
+            if(current.right){
+                current = current.right
+            }else{
+                return this.Max(current.left)
+            }
+        }
+    }
+
+
+    // Validate Is Given Tree is BST
+
+    isBST(node = this.root, min = -Infinity, max = Infinity){
+        if(!node) return true
+
+        if(node.value < min || node.value > max) return false
+
+        return this.isBST(node.left, min, node.value)
+            && this.isBST(node.right, node.value, max)
+    }
 }
 
 const bst = new BinarySearchTree()
@@ -90,7 +190,27 @@ console.log(bst.search(bst.root,10))
 console.log(bst.search(bst.root,5))
 console.log(bst.search(bst.root,154))
 
+bst.preOrder(bst.root)
+console.log("////////")
+bst.inOrder(bst.root)
+console.log("////////")
+bst.postOrder(bst.root)
 
+ bst.levelOrder()
+console.log(bst.Min(bst.root))
+console.log(bst.Max(bst.root))
+console.log("//////////")
+bst.delete(15)
+bst.levelOrder()
+console.log("//////////")
+
+console.log(bst.Height(bst.root))
+
+console.log("nth lage",bst.NthLargest(4))
+console.log("second largest", bst.SecondLargest())
+
+console.log("is BST-")
+console.log(bst.isBST())
 
 ///////////////////////////////
 
