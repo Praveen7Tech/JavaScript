bddbf1f4f126a8291f4eb32d107404cdd7a5df19
Pr4veenn <praveen.tech.js@gmail.com>
2025-07-19 18:00:40 +0530

    BST operations

--- DSA/BST.js
+++ DSA/BST.js
@@ -1,63 +1,71 @@
 
-class Node {
+class Node{
     constructor(value){
         this.value = value
         this.left = null
-        this.right = null       
+        this.right = null
     }
 }
 
-class BinarySearchTree {
+class BST{
     constructor(){
         this.root = null
     }
-
-    isEmpty(){
-        return this.root === null
-    }
-
+    
     insert(value){
         const node = new Node(value)
-        if(this.isEmpty()){
+        if(this.root == null){
             this.root = node
         }else{
             this.insertNode(this.root, node)
         }
     }
-
+    
     insertNode(root, node){
         if(node.value < root.value){
-            if(root.left === null){
+            if(root.left == null){
                 root.left = node
             }else{
                 this.insertNode(root.left, node)
             }
         }else{
-            if(root.right === null){
+            if(root.right == null){
                 root.right = node
             }else{
                 this.insertNode(root.right, node)
             }
         }
     }
-
-    search(root, value){
-        if(!root){
-            return false
-        }else{
-            if(root.value === value){
-                return true
+    
+    preorder(root){
+        if(root){
+            console.log(root.value)
+            this.preorder(root.left)
+            this.preorder(root.right)
+        }
+    }
+    inorder(root){
+        if(root){
+            this.inorder(root.left)
+            console.log(root.value)
+            this.inorder(root.right)
+        }
+    }
+    
+    levelorder(root){
+        let queue = [root]
+        while(queue.length){
+            let curr = queue.shift()
+            console.log(curr.value)
+            if(curr.left){
+                queue.push(curr.left)
             }
-            else if(value < root.value){
-                return this.search(root.left, value)
-            }
-            else{
-                return this.search(root.right, value)
+            if(curr.right){
+                queue.push(curr.right)
             }
         }
     }
-
-    // MIN - VALUE (BST)
+    
     Min(root){
         if(!root.left){
             return root.value
@@ -65,8 +73,7 @@ class BinarySearchTree {
             return this.Min(root.left)
         }
     }
-
-    // MAX - VALUE (BST)
+    
     Max(root){
         if(!root.right){
             return root.value
@@ -74,144 +81,34 @@ class BinarySearchTree {
             return this.Max(root.right)
         }
     }
-
-    //DELETE NODE
-    delete(value){
-        this.root = this.deleteNode(this.root, value)
-    }
-
-    deleteNode(root, value){
-        if(root === null){
-            return null
-        }
-        if(value < root.value){
-            root.left = this.deleteNode(root.left, value)
-        }
-        else if(value > root.value){
-            root.right = this.deleteNode(root.right, value)
-        }
-        else{
-            if(!root.left && !root.right){
-                return null
-            }
-            if(!root.left){
-                return root.right
-            }
-            else if(!root.right){
-                return root.left
-            }
-
-            root.value = this.Min(root.right)
-            root.right = this.deleteNode(root.right, root.value)
-        }
-        return root
-    }
-
-    // Height of BST 
-    Height(root){
-        if(root === null){
-            return -1
-        }
-
-        const leftHeight = this.Height(root.left)
-        const rightHeight = this.Height(root.right)
-
-        return 1 + Math.max(leftHeight, rightHeight)
-    }
-
-    // Find Nth largest
-    NthLargest(n){
-        this.count = 0
-        this.nthLargest = null
-        this.FindNthLarge(this.root, n)
-        return this.nthLargest
-    }
-
-    FindNthLarge(root, n){
-        if(!root){
-            return
-        }
-        this.FindNthLarge(root.right,n)
-        this.count++
-
-        if(this.count === n){
-            this.nthLargest = root.value
-        }
-        this.FindNthLarge(root.left, n)
-    }
-
-    
-    // Find Second largest
-    SecondLargest(){
-        if(!this.root || !this.root.left && !this.root.right){
-            return null
-        }
-
-        let current = this.root
-        while(current){
-            if(current.right && !current.right.right && !current.right.left){
-                return current.value
-            }
-
-            if(current.right && !current.right.right && current.right.left){
-                return this.Max(current.right.left)
-            }
-
-            if(current.right){
-                current = current.right
-            }else{
-                return this.Max(current.left)
-            }
-        }
-    }
-
-
-    // Validate Is Given Tree is BST
-
-    isBST(node = this.root, min = -Infinity, max = Infinity){
-        if(!node) return true
-
-        if(node.value < min || node.value > max) return false
-
-        return this.isBST(node.left, min, node.value)
-            && this.isBST(node.right, node.value, max)
-    }
 }
 
-const bst = new BinarySearchTree()
+const bst = new BST()
 
 bst.insert(10)
 bst.insert(5)
 bst.insert(15)
-bst.insert(3)
 bst.insert(7)
+bst.insert(12)
+bst.insert(3)
 
-console.log(bst.search(bst.root,10))
-console.log(bst.search(bst.root,5))
-console.log(bst.search(bst.root,154))
+bst.preorder(bst.root)
+console.log("///////////")
+bst.inorder(bst.root)
+console.log("///////////")
+bst.levelorder(bst.root)
 
-bst.preOrder(bst.root)
-console.log("////////")
-bst.inOrder(bst.root)
-console.log("////////")
-bst.postOrder(bst.root)
-
- bst.levelOrder()
+console.log("///////////")
 console.log(bst.Min(bst.root))
 console.log(bst.Max(bst.root))
-console.log("//////////")
-bst.delete(15)
-bst.levelOrder()
-console.log("//////////")
 
-console.log(bst.Height(bst.root))
 
-console.log("nth lage",bst.NthLargest(4))
-console.log("second largest", bst.SecondLargest())
 
-console.log("is BST-")
-console.log(bst.isBST())
 
-///////////////////////////////
+
+
+
+
+
 
 
