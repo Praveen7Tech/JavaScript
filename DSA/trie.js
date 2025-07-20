4436cb3f47898071fb81b65e1ee20ab7d27b2110
Pr4veenn <praveen.tech.js@gmail.com>
2025-07-20 22:59:53 +0530

    trie workouts

--- DSA/trie.js
+++ DSA/trie.js
@@ -1,7 +1,94 @@
 
+// class TrieNode {
+//     constructor(){
+//         this.child = {}
+//         this.isEnd = false
+//     }
+// }
+
+// class Trie {
+//     constructor(){
+//         this.root = new TrieNode()
+//     }
+
+//     insert(word){
+//         let node = this.root
+//         for(let char of word){
+//             if(!node.child[char]){
+//                 node.child[char] = new TrieNode()
+//             }
+//             node = node.child[char]
+//         }
+//         node.isEnd = true
+//     }
+
+//     search(word){
+//         let node = this.root
+//         for(let char of word){
+//             if(!node.child[char]){
+//                 return false
+//             }
+//             node = node.child[char]
+//         }
+//         return node.isEnd
+//     }
+
+//     startsWith(prefix){
+//         let node = this.root
+//         for(let char of prefix){
+//             if(!node.child[char]){
+//                 return null // false
+//             }
+//             node = node.child[char]
+//         }
+//         return node // true
+//     }
+
+
+//     autoComplete(prefix){
+//         const words = []
+//         const startNode = this.startsWith(prefix)
+
+//         if(!startNode){
+//             return words
+//         }
+
+//         const dfs = (node, currentWord) => {
+//             if(node.isEnd){
+//                 words.push(currentWord)
+//             }
+
+//             for(let char in node.child){
+//                 dfs(node.child[char], currentWord + char)
+//             }
+//         }
+//         dfs(startNode, prefix)
+//         return words
+//     }
+
+// }
+
+// const trie = new Trie()
+
+// trie.insert("cat");
+// trie.insert("can");
+// trie.insert("camera");
+// trie.insert("car");
+// trie.insert("dog");
+
+// console.log(trie.search("car"));
+// console.log(trie.autoComplete("ca")); 
+// console.log(trie.autoComplete("cam")); 
+// console.log(trie.autoComplete("d")); 
+
+
+///////////////////////////
+
+
+// TRIE
 class TrieNode {
     constructor(){
-        this.child = {}
+        this.child = []
         this.isEnd = false
     }
 }
@@ -33,22 +120,20 @@ class Trie {
         return node.isEnd
     }
 
-    startsWith(prefix){
+    startWith(prefix){
         let node = this.root
         for(let char of prefix){
             if(!node.child[char]){
-                return null // false
+                return null
             }
             node = node.child[char]
         }
-        return node // true
+        return node
     }
 
-
     autoComplete(prefix){
-        const words = []
-        const startNode = this.startsWith(prefix)
-
+        let words = []
+        let startNode = this.startWith(prefix)
         if(!startNode){
             return words
         }
@@ -57,7 +142,6 @@ class Trie {
             if(node.isEnd){
                 words.push(currentWord)
             }
-
             for(let char in node.child){
                 dfs(node.child[char], currentWord + char)
             }
@@ -65,23 +149,17 @@ class Trie {
         dfs(startNode, prefix)
         return words
     }
-
 }
 
 const trie = new Trie()
 
-trie.insert("cat");
-trie.insert("can");
-trie.insert("camera");
-trie.insert("car");
-trie.insert("dog");
+trie.insert("cat")
+trie.insert("car")
+trie.insert("cap")
+trie.insert("camera")
 
-console.log(trie.search("car"));
-console.log(trie.autoComplete("ca")); 
-console.log(trie.autoComplete("cam")); 
-console.log(trie.autoComplete("d")); 
+console.log(trie.search("car"))
+console.log(trie.startWith("cam"))
 
-
-///////////////////////////
-
-
+// console.log(trie.autoComplete("cam"))
+// console.log(trie.autoComplete("ca"));
