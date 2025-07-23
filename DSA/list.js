7dcaf11570cf3d1e0d3a58c8d94c50e326528b76
Pr4veenn <praveen.tech.js@gmail.com>
2025-07-23 21:05:31 +0530

    list operations

--- DSA/list.js
+++ DSA/list.js
@@ -48,7 +48,62 @@ class LinkedList {
         this.size++
     }   
 
-   
+    //insert a value to a specific positiom
+    insert(value, index){
+        if(index < 0 || index > this.size){
+            return
+        }
+        if(index === 0){
+            this.prepend(value)
+        }else{
+            const node = new Node(value)
+            let prev = this.head
+            for(let i=0; i< index-1;i++){
+                prev = prev.next
+            }
+            node.next =prev.next;
+            prev.next =node;
+            this.size++
+        }
+    }
+
+    //Remove a value fron specified index
+    Remove(index){  
+        if(index < 0 || index >= this.size){
+            return null
+        }
+        let removeNode;
+        if(index === 0){
+            removeNode = this.head;
+            this.head = this.head.next
+        }else{
+            let prev = this.head
+            for(let i=0; i< index-1; i++){
+                prev = prev.next
+            }
+            removeNode = prev.next;
+            prev.next = removeNode.next
+        }
+        this.size--;
+        return removeNode.value
+    }
+
+    // remove a matching value from linked list
+    delete(value){
+        if(value === this.head.value){
+            this.head = this.head.next
+        }else{
+            let prev = this.head
+            while(prev.next && prev.next.value !== value){
+                prev = prev.next
+            }
+            if(prev.next){
+                let removeValue = prev.next
+                prev.next = removeValue.next
+            }
+        }
+        this.size--
+    }
 
     print(){
         if(this.isEmpty()){
@@ -75,6 +130,20 @@ list.append(30)
 list.append(40)
 list.append(50)
 list.print()
+
+list.deleteNthNode(9)
+list.print()
+console.log(list.searchValue(10))
+
+console.log("insert ------------");
+
+list.print()
+
+list.insert(10, 0)
+list.print()
+
+
+
 
 
 
