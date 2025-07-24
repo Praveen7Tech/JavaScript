730dd472dc41a9cba5721957d18f294aa0f2cb20
Pr4veenn <praveen.tech.js@gmail.com>
2025-07-24 20:03:21 +0530

    JS

--- practice/practice.js
+++ practice/practice.js
@@ -167,3 +167,94 @@
 // x.map((n)=>{
 //     console.log(n.firstName)
 // })
+
+////////////////////////
+
+
+
+//   // callback ///
+
+//   function call(){
+//     console.log("call me");
+//   }
+
+//   function main(callback){
+//     console.log("first");
+
+//     setTimeout(()=>{
+        
+//         callback();
+//         console.log("finish");
+//     },3000)
+    
+//   }
+
+//   main(call)
+
+//   function call(){
+//     console.log("call me..");
+    
+//   }
+
+//   function main (callback){
+
+//     console.log('start')
+
+//     setTimeout(()=>{
+//         callback ()
+//         console.log("finish")
+//     },3000)
+//   }
+
+//   main(call)
+
+
+//   function factorial(n){
+     
+//     let result=1;
+//     for(let i=1;i<=n;i++){
+//         result *= i;
+//     }
+//     return result;
+//   }
+
+//   console.log(factorial(10));
+
+//   const person = {
+//     name: "John",
+//     greet: function (message) {
+//       console.log(this.name +" says : "+ message );
+//     },
+//   };
+  
+//   const anotherPerson = {
+//     name: "Alice",
+//   };
+  
+//   person.greet.call(anotherPerson, " Hello!"); // Alice says: Hello!
+
+//   'use strict';
+
+//   var num = 01234;
+
+//   console.log(num);
+
+
+//   function sum(...numbers){
+//     let result=0;
+//     for(let num of numbers){
+//       result+= num;
+//     }
+//     return result;
+//   }
+
+//   console.log(sum(1,2,3));
+
+//   const abc={
+//     var x=10,
+//     fn : function(){
+//         console.log(this.name)
+//     }
+// };
+// abc.fn();
+
